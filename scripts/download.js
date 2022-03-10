26a791c5fac9d3e49dd8de49842069fae00ab0cb
Ivan Petrov <96638180+kgbs-py@users.noreply.github.com>
2022-03-10 06:34:47 +0300

    fix download script

--- scripts/download.js
+++ scripts/download.js
@@ -8,14 +8,10 @@ import util from 'util'
 
 process.chdir(path.join(path.dirname(process.argv[1]), '..'))
 
-let opts = {
-  cookies: {
-    wsauth_token: ini.parse(await fs.readFile('./scripts/.tokens', 'utf8')).wsauth_token
-  }
-}
-
-async function get(team) {
-  let result = await needle('get', 'https://clans.worldofwarships.eu/api/ladder/battles/?team=' + team, opts)
+async function get(team, wsauth_token) {
+  let result = await needle('get', 'https://clans.worldofwarships.eu/api/ladder/battles/?team=' + team, {
+    cookies: { wsauth_token }
+  })
   assert.strictEqual(result.statusCode, 200)
   assert.ok(Array.isArray(result.body))
   console.log(`fetched team${team} data, ${result.body.length} entries`)
@@ -48,20 +44,20 @@ async function process_data(data, teamid) {
   for (let battle of data) {
     let [ , date, time ] = battle.finished_at.match(/^(\d\d\d\d-\d\d-\d\d)T(\d\d:\d\d:\d\d)\+00:00$/)
 
-    let index = []
+    //let index = []
     let db = {}
-    let filename = `${date}.json`
+    let filename = `data/${battle.season_number}.${battle.teams[0].claninfo.tag.toLowerCase()}/${date}.json`
     let key = `${time}-${team}`
 
-    if (await fs_exists('data/0000-index.json')) {
-      index = JSON.parse(await fs.readFile('data/0000-index.json'))
+    //if (await fs_exists('data/0000-index.json')) {
+    //  index = JSON.parse(await fs.readFile('data/0000-index.json'))
+    //}
+
+    if (await fs_exists(filename)) {
+      db = JSON.parse(await fs.readFile(filename))
     }
 
-    if (await fs_exists('data/' + filename)) {
-      db = JSON.parse(await fs.readFile('data/' + filename))
-    }
-
-    assert.strictEqual(battle.teams[0].claninfo.tag, 'H-O-E')
+    //assert.strictEqual(battle.teams[0].claninfo.tag, 'H-O-E')
     if (db[key]) continue
 
     for (let teams of battle.teams) {
@@ -70,21 +66,28 @@ async function process_data(data, teamid) {
       }
     }
 
-    console.log(`new battle, ${date} ${time}, ${team}, ${battle.teams[0].result}`)
+    console.log(`new battle, ${filename} ${time}, ${team}, ${battle.teams[0].result}`)
     db[key] = battle
-    await fs.writeFile('data/' + filename, JSON.stringify(sort_object(db), null, 2) + '\n')
+    await fs.writeFile(filename, JSON.stringify(sort_object(db), null, 2) + '\n')
 
-    if (!Object.values(index).slice(-1)[0].files.includes(filename)) {
+    /*if (!Object.values(index).slice(-1)[0].files.includes(filename)) {
       Object.values(index).slice(-1)[0].files.push(filename)
       Object.values(index).slice(-1)[0].files.sort()
       await fs.writeFile('data/0000-index.json', JSON.stringify(index, null, 2) + '\n')
-    }
+    }*/
   }
 }
 
-await process_data(await get(1), 1)
-await process_data(await get(2), 2)
+let iniconfig = ini.parse(await fs.readFile('./scripts/.tokens', 'utf8'))
 
-let pkg = JSON.parse(await fs.readFile('package.json'))
+for (let i = 0; ; i++) {
+  let token = iniconfig['wsauth_token.' + String(i)]
+  if (!token) break
+
+  await process_data(await get(1, token), 1)
+  await process_data(await get(2, token), 2)
+}
+
+/*let pkg = JSON.parse(await fs.readFile('package.json'))
 pkg.integrity = (await util.promisify(child_process.exec)('sha1sum data/*json | sha1sum')).stdout.slice(0, 40)
-await fs.writeFile('package.json', JSON.stringify(pkg, null, 2) + '\n')
+await fs.writeFile('package.json', JSON.stringify(pkg, null, 2) + '\n')*/
